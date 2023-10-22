b960be541738361ec427c01f58caf9b92ff583e9
Norbert Varga <norbika0210@gmail.com>
2023-10-22 14:43:05 +0200

    elso

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 import './style.css'
 import 'bootstrap/dist/css/bootstrap-grid.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+const url=document.getElementById('url');
+const szovegmeret= document.getElementById('szovegmeret');
+url.addEventListener('keyup',urlmod);
+szovegmeret.addEventListener('change',szovegmeretvalt);
+
+function urlmod()
+{
+  let url=document.getElementById("url").value
+  document.getElementById("img").src=url
+
+}
+
+function szovegmeretvalt(){
+    var meret=document.getElementById('szovegmeret').value;
+    meret=parseFloat(meret);
+    const text=document.getElementById('szoveg');
+    text.style.fontSize=meret+"px";
+}
